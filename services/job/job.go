ec841b9acd38ecf3b7ed292bfd1908171316674a
Fenrich Alessandro <3001068@stud.hs-mannheim.de>
2025-05-01 08:04:39 +0000

    added some comments to explain possible values

--- services/job/job.go
+++ services/job/job.go
@@ -7,10 +7,10 @@ type JobStatus string
 const (
 	StatusQueued    JobStatus = "queued"    // default value for new job
 	StatusScheduled JobStatus = "scheduled" // is set as soon as a worker has been assigned (by the job-scheduler)
-	StatusRunning   JobStatus = "running"
-	StatusCompleted JobStatus = "completed"
-	StatusFailed    JobStatus = "failed"
-	StatusCancelled JobStatus = "cancelled"
+	StatusRunning   JobStatus = "running"   // set by daemon
+	StatusCompleted JobStatus = "completed" // set by daemon
+	StatusFailed    JobStatus = "failed"    // set by daemon
+	StatusCancelled JobStatus = "cancelled" // set by daemon
 )
 
 type ContainerImage struct {
@@ -23,7 +23,7 @@ type ContainerImage struct {
 type Job struct {
 	ID                   string            `json:"id"`      // generated as UUID
 	JobName              string            `json:"jobName"` //set by User
-	UserID               string            `json:"userId"`
+	UserID               string            `json:"userId"`  // get from JWT
 	Image                ContainerImage    `json:"image"`
 	AdjustmentParameters map[string]string `json:"parameters"` // e.g key(-p) : value (8080:8080)
 	Status               JobStatus         `json:"status"`
@@ -32,7 +32,7 @@ type Job struct {
 	Result               string            `json:"result"` // perhaps some containers will provide a result
 	WorkerID             string            `json:"workerId"`
 	ErrorMessage         string            `json:"errorMessage"`
-	ComputeZone          string            `json:"computeZone"`     // zone key we get from Electricity Maps API, e.g "DE" (germany)
-	CarbonIntensity      int               `json:"carbonIntensity"` // CO2eq/kWh which are emitted while executing job
+	ComputeZone          string            `json:"computeZone"`     // saved as "zone key", we get from Electricity Maps API, e.g "DE" (germany)
+	CarbonIntensity      int               `json:"carbonIntensity"` // CO2eq/kWh which are emitted during job execution
 	CarbonSaving         int               `json:"carbonSavings"`   // consumption savings compared to the actual consumer location
 }
