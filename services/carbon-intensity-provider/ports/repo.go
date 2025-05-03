34e6ffed2d929a7c78ea7869ccabed65f3b1c4ae
Yodkhatha Bick <yodkhatha@gmail.com>
2025-05-03 21:39:58 +0000

    removed head

--- services/carbon-intensity-provider/ports/repo.go
+++ services/carbon-intensity-provider/ports/repo.go
@@ -2,7 +2,6 @@ package ports
 
 import (
 	"context"
-<<<<<<< HEAD
 	"errors"
 
 	"github.com/informatik-mannheim/cmg-ss2025/services/carbon-intensity-provider/model"
@@ -14,11 +13,4 @@ type Repo interface {
 	Store(data model.CarbonIntensityData, ctx context.Context) error
 	FindById(id string, ctx context.Context) (model.CarbonIntensityData, error)
 	FindAll(ctx context.Context) ([]model.CarbonIntensityData, error)
-=======
-)
-
-type Repo interface {
-	Store(carbonIntensityProvider CarbonIntensityProvider, ctx context.Context) error
-	FindById(id string, ctx context.Context) (CarbonIntensityProvider, error)
->>>>>>> origin
 }
