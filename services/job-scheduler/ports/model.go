cddc462aac14eabfd88355f0b1a4711cacca52ae
-T <36013370+Tonka-de@users.noreply.github.com>
2025-05-12 17:05:52 +0200

    102 fixjob scheduler remove dependencies (#104)
    * fix(job-scheduler): use local datatypes instead of those of other services
    
    * feat(job-scheduler): add response types for requests
    
    * fix(job-scheduler): cleaner code
    
    * fix(job-scheduler): remove unneccessary attributes from struct
    
    * fix(job-scheduler): UpdateWorkerPayload wrong json

--- services/job-scheduler/ports/model.go
+++
@@ -1,7 +0,0 @@
-package ports
-
-// model.go - For possible future interaction (like metrics or CLI)
-
-type Environments struct {
-	Interval int
-}
--- services/job-scheduler/ports/notifier.go
+++ services/job-scheduler/ports/notifier.go
@@ -1,35 +1,28 @@
 package ports
 
-import (
-	CarbonIntensityProvider "github.com/informatik-mannheim/cmg-ss2025/services/carbon-intensity-provider/model"
-	Job "github.com/informatik-mannheim/cmg-ss2025/services/job"
-	WorkerRegistry "github.com/informatik-mannheim/cmg-ss2025/services/worker-registry/ports"
-)
-
-// FIXME: Change id-string to id-uuid, currently not possible due to lack of
-// uuid package approval...
+import "github.com/informatik-mannheim/cmg-ss2025/services/job-scheduler/model"
 
 type UpdateJob struct {
-	ID              string  `json:"id"`       // actually uuid
-	WorkerID        string  `json:"workerId"` // actually uuid
-	ComputeZone     string  `json:"computeZone"`
-	CarbonIntensity float64 `json:"carbonIntensity"`
-	CarbonSavings   float64 `json:"carbonSavings"`
+	ID              string  `json:"id"`              // FIXME: actually UUID
+	WorkerID        string  `json:"workerId"`        // FIXME: actually UUID
+	ComputeZone     string  `json:"computeZone"`     //
+	CarbonIntensity float64 `json:"carbonIntensity"` //
+	CarbonSavings   float64 `json:"carbonSavings"`   //
 	// No status on this struct, because there is only 1 possible option,
 	// so the function will set it itself.
 }
 
 type UpdateWorker struct {
-	ID string `json:"id"` // actually uuid
+	ID string `json:"id"` // FIXME: actually UUID
 	// No status on this struct, because there is only 1 possible option,
 	// so the function will set it itself.
 }
 
 type Notifier interface {
 	// -- Getters --
-	GetJobs() ([]Job.Job, error)
-	GetWorkers() ([]WorkerRegistry.Worker, error)
-	GetCarbonIntensities(zones []string) ([]CarbonIntensityProvider.CarbonIntensityData, error)
+	GetJobs() (model.GetJobsResponse, error)
+	GetWorkers() (model.GetWorkersResponse, error)
+	GetCarbonIntensities(zones []string) (model.CarbonIntensityResponse, error)
 
 	// -- Setters --
 	AssignJob(update UpdateJob) error
