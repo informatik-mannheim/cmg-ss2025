98128abd207a128f6e1fe7b8152b31dec9c32dd1
Fenrich Alessandro <3001068@stud.hs-mannheim.de>
2025-05-14 18:32:22 +0000

    refactor: old cumbersome version replaced by a modern one

--- services/job/utils/job_utils.go
+++ services/job/utils/job_utils.go
@@ -1,16 +1,13 @@
 package utils
 
 import (
+	"slices"
+
 	"github.com/informatik-mannheim/cmg-ss2025/services/job/ports"
 )
 
 // containsStatus checks if a given status is present in the status list.
 // It returns true if the status is found, otherwise false.
 func ContainsStatus(statusList []ports.JobStatus, status ports.JobStatus) bool {
-	for _, s := range statusList {
-		if s == status {
-			return true
-		}
-	}
-	return false
+	return slices.Contains(statusList, status)
 }
