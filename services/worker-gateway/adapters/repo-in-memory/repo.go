5c92d350262370f2af06f0c89d35d344250b98ce
danp117 <96053929+danp117@users.noreply.github.com>
2025-05-15 20:09:46 +0200

    Worker Gateway Service Core (#113)
    * Worker Gateway Service folders and files without any implementations yet (#93)
    
    * Renaming core files as review requested
    
    * Worker Gateway Core (#106)
    
    * Better error handling so tests passes (#106)

--- services/worker-gateway/adapters/repo-in-memory/repo.go
+++ services/worker-gateway/adapters/repo-in-memory/repo.go
@@ -1,20 +1,5 @@
 package repo_in_memory
 
-import (
-	"context"
-
-	"github.com/informatik-mannheim/cmg-ss2025/services/worker-gateway/ports"
-)
-
 type Repo struct {
 }
 
-var _ ports.Repo = (*Repo)(nil)
-
-func NewRepo() *Repo {
-	return nil
-}
-
-func (r *Repo) Store(ectx context.Context) error {
-	return nil
-}
