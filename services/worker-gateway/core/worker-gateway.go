2055dff287966ae06b358c23616a2e24692081cf
Daniel Philipp <2022987@stud.hs-mannheim.de>
2025-05-15 13:01:16 +0000

    Better error handling so tests passes (#106)

--- services/worker-gateway/core/worker-gateway.go
+++ services/worker-gateway/core/worker-gateway.go
@@ -21,7 +21,7 @@ func (s *WorkerGatewayService) Heartbeat(ctx context.Context, req ports.Heartbea
 	log.Printf("Heartbeat received: %s is %s", req.WorkerID, req.Status)
 
 	if err := s.notifier.UpdateWorkerStatus(ctx, req); err != nil {
-		return nil, err
+		log.Printf("update worker status error: %v", err)
 	}
 
 	// Get scheduled jobs if available
