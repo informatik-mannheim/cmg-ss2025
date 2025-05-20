369e52a826274962665eefe5962e15cdce055cef
LilliEllaK <147616661+LilliEllaK@users.noreply.github.com>
2025-05-20 10:59:54 +0200

    Removed all register endpoints, structs and related contents that were no longer up to date. (#157)

--- services/consumer-gateway/ports/api.go
+++ services/consumer-gateway/ports/api.go
@@ -56,16 +56,3 @@ type ZoneRequest struct {
 type ZoneResponse struct {
 	Zone string `json:"zone"`
 }
-
-type ZoneClient interface {
-	GetZone(ctx context.Context, req ZoneRequest) (ZoneResponse, error)
-}
-
-type LoginClient interface {
-	Login(ctx context.Context, req ConsumerLoginRequest) (LoginResponse, error)
-}
-
-type JobClient interface {
-	GetJobOutcome(ctx context.Context, jobID string) (JobOutcomeResponse, error)
-	CreateJob(ctx context.Context, req CreateJobRequest) (CreateJobResponse, error)
-}
