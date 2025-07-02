698dce9f5b4a3d5011b8e0bcacc5af72d19916c1
danp117 <96053929+danp117@users.noreply.github.com>
2025-07-02 18:53:16 +0200

    Implement run compute job + tests (#264)

--- services/worker-gateway/ports/job-service.go
+++ services/worker-gateway/ports/job-service.go
@@ -8,3 +8,18 @@ type JobService interface {
 	UpdateJob(ctx context.Context, req ResultRequest) error
 	FetchScheduledJobs(ctx context.Context) ([]Job, error)
 }
+
+type Job struct {
+	ID                   string            `json:"id"`
+	WorkerID             string            `json:"workerId"`
+	Image                ContainerImage    `json:"image"`
+	AdjustmentParameters map[string]string `json:"adjustmentParameters"`
+	Status               string            `json:"status"`
+	Result               string            `json:"result"`
+	ErrorMessage         string            `json:"errorMessage"`
+}
+
+type ContainerImage struct {
+	Name    string `json:"name" db:"image_name"`
+	Version string `json:"version" db:"image_version"`
+}
