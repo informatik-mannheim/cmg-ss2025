fc5acb62e455db3d467809c675dd710b5fb7d853
Khlui <124293078+YoBiKhu@users.noreply.github.com>
2025-07-03 20:53:51 +0200

    277 removal of dead code (#297)
    * removed dead code and also fixed the readme
    
    * removed dead code and also fixed the readme
    
    * removed notifier
    
    * removed notifier
    
    * removed notifier
    
    * removed notifier
    
    * removed notifier
    
    * removed notifier
    
    * removed notifier
    
    * removed notifier
    
    * removed notifier
    
    * removed notifier
    
    * removed notifier
    
    * removed notifier
    
    * removed notifier
    
    * removed notifier
    
    * removed hash
    
    ---------
    
    Co-authored-by: SanFenDev <144385762+SanFenDevelop@users.noreply.github.com>

--- services/user-management/ports/notifier.go
+++
@@ -1,9 +0,0 @@
-package ports
-
-import "context"
-
-type Notifier interface {
-	UserRegistered(id string, role string, ctx context.Context)
-	UserLoggedIn(id string, ctx context.Context)
-	Event(message string, ctx context.Context)
-}
